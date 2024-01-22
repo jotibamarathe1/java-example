dfb59287deae1feb7fb9092beea7561d9a3920fe
jotiba <jotiba.marathe1@gmail.com>
2024-01-22 18:41:04 +0530

    operator demo

---
+++ java-practice/src/com/jotiba/practice/basicJava/operator/InstanceOfOperatorDemo.java
@@ -0,0 +1,31 @@
+package com.jotiba.practice.basicJava.operator;
+
+public class InstanceOfOperatorDemo {
+
+    public static void main(String[] args) {
+        String city = "pune";
+        Integer length = 23;
+        Double temperature = 36.5;
+
+        if(city instanceof String){
+            System.out.println("type of city is String");
+        }
+        else{
+            System.out.println("type of city is not String");
+        }
+
+        if(length instanceof Integer){
+            System.out.println("type of length is integer");
+        }
+        else{
+            System.out.println("type of length is not integer");
+        }
+
+        if(temperature instanceof Double){
+            System.out.println("type of temperature is double");
+        }
+        else{
+            System.out.println("type of temperature is not double");
+        }
+    }
+}
---
+++ java-practice/src/com/jotiba/practice/basicJava/operator/LeftShiftOperatorDemo.java
@@ -0,0 +1,14 @@
+package com.jotiba.practice.basicJava.operator;
+import java.util.Scanner;
+
+public class LeftShiftOperatorDemo {
+    /*Psuedo code : write a program to shift the bit by 4 to left*/
+
+    public static void main(String[] args) {
+        System.out.println("Please enter value");
+        Scanner sc = new Scanner(System.in);
+        int input = Integer.valueOf(sc.nextLine());
+
+        System.out.println("result: " + (input << 4));
+    }
+}
