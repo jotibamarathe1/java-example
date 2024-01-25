4f3942617bae71747cc63fd288aa46ed5314df15
jotiba <jotiba.marathe1@gmail.com>
2024-01-25 11:51:13 +0530

    deleted

--- java-practice/src/com/jotiba/practice/coreJava/oopsConcept/model/Address.java
+++
@@ -1,5 +0,0 @@
-package com.jotiba.practice.coreJava.oopsConcept.model;
-
-public class Address {
-    public String city;
-}
--- java-practice/src/com/jotiba/practice/coreJava/oopsConcept/model/Programmer.java
+++
@@ -1,5 +0,0 @@
-package com.jotiba.practice.coreJava.oopsConcept.model;
-
-public class Programmer extends Employee{
-  public String workingLanguage;
-}
