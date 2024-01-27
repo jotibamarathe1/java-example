2844647cfe3fba6b5117beec63d533d3b6dab4b9
jotiba <jotiba.marathe1@gmail.com>
2024-01-27 23:28:40 +0530

    armstrong numbers

---
+++ java-practice/src/com/jotiba/practice/logicDevelopment/assignment/ArmstrongNumber.java
@@ -0,0 +1,26 @@
+package com.jotiba.practice.logicDevelopment.assignment;
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a range");
+        int number = sc.nextInt();
+
+        System.out.println("armstrong number are: ");
+        for (int i = 0; i <= number; i++) {
+            int sum = 0;
+            int c = 0;
+            int temp = i;
+            int remainder = 0;
+            while (temp != 0) {
+                remainder = temp % 10;
+                c = (int) (Math.pow(remainder, 3));
+                sum = sum + c;
+                temp = temp / 10;
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
