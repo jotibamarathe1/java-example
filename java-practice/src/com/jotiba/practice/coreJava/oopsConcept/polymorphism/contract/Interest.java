3ae09b511ebeb8ee50d62604e23ab92615db1735
jotiba <jotiba.marathe1@gmail.com>
2024-01-27 20:11:13 +0530

    deleted

--- java-practice/src/com/jotiba/practice/coreJava/oopsConcept/polymorphism/contract/Interest.java
+++
@@ -1,6 +0,0 @@
-package com.jotiba.practice.coreJava.oopsConcept.polymorphism.contract;
-
-public interface Interest {
-     double calculate(double interestRate, int principleAmount, double years);
-     double calculate(double interestRate, int principleAmount);
-}
