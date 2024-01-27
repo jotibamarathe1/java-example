3b5ca700fd4fa8099b1413e81ad1ae251814363d
jotiba <jotiba.marathe1@gmail.com>
2024-01-27 22:15:14 +0530

    classes and objects

---
+++ java-practice/src/com/jotiba/practice/coreJava/oopsConcept/ObjectsAndClasses/Example2.java
@@ -0,0 +1,17 @@
+package classes;
+
+class Student1{
+	String name;
+	int rollNumber;
+}
+
+public class Example2 {
+    public static void main(String args[]) {
+	Student1 s1 = new Student1();
+	s1.name="s1";
+	s1.rollNumber=1;
+	
+	System.out.println(s1.name);
+	System.out.println(s1.rollNumber);
+}
+}
