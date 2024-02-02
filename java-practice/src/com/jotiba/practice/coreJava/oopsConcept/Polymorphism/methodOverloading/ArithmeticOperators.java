4bc1dfe20f36a61c1ae898a8bb738a10f566cb17
jotiba <jotiba.marathe1@gmail.com>
2024-02-02 14:23:04 +0530

    example on method overloading

---
+++ java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Polymorphism/methodOverloading/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package overloading;
+
+public class ArithmeticOperators {
+	int n1;
+	int n2;
+	
+	public int sum(int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	public int sum(int n1, int n2, int n3) {
+		return n1 - n2 - n3;
+	}
+	
+	public double sum(int n1, int n2, double n3) {
+		return n1 * n2 * n3;
+	}
+	
+	public double sum(double n1, int n2, int n3) {
+		return n1 / n2 / n3;
+	}
+}
