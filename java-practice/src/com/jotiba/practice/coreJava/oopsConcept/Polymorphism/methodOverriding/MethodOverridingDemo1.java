50b27f8187a562b93c778580857cf725937156c2
jotiba <jotiba.marathe1@gmail.com>
2024-02-15 10:53:30 +0530

    program on method overriding

---
+++ java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Polymorphism/methodOverriding/MethodOverridingDemo1.java
@@ -0,0 +1,21 @@
+package overriding;
+
+class Language {
+	  public void displayInfo() {
+	    System.out.println("Common English Language");
+	  }
+	}
+
+	class Java extends Language {
+	  @Override
+	  public void displayInfo() {
+	    System.out.println("Java Programming Language");
+	  }
+	}
+
+public class MethodOverridingDemo1{
+	public static void main(String args[]) {
+	    Java j1 = new Java();
+	    j1.displayInfo();
+	}
+}
