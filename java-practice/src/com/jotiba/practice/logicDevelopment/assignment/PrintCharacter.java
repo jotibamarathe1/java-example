ffc0cd6e5eb8c6727b8568f0fd5949577bea17f7
jotiba <jotiba.marathe1@gmail.com>
2024-02-17 18:36:04 +0530

    program on alphabate

---
+++ java-practice/src/com/jotiba/practice/logicDevelopment/assignment/PrintCharacter.java
@@ -0,0 +1,13 @@
+package com.jotiba.practice.logicDevelopment.assignment;
+
+public class PrintCharacter {
+
+    public static void main(String[] args) {
+        //write a program to print alphabets from A to Z.
+
+        System.out.println("Alphabets from A to Z");
+           for(char ch = 'A'; ch <= 'Z'; ch++){
+               System.out.println(ch);
+           }
+    }
+}
