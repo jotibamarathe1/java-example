889b197cb6738ecb3df4b99419f4a75b6b7326da
jotiba <jotiba.marathe1@gmail.com>
2024-02-24 12:03:08 +0530

    example on interface added

--- java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Interface/InterfaceDemo1/InterfaceDemo1.java
+++ java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Interface/InterfaceDemo1/InterfaceDemo1.java
@@ -5,15 +5,13 @@ interface Printable{
 }
 
 class Article implements Printable{
-
-	@Override
+  @Override
 	public void print() {
-		System.err.println("printing");
-		}
+	  System.err.println("printing");
+	}
 }
 public class InterfaceDemo1 {
 	public static void main(String args[]) {
-	  //Article p = new Article();
 		Printable p = new Article();
 		p.print();
 	}
