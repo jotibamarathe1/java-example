f164851a9856e4d7e073b8e6b11dcc5bb266f680
jotiba <jotiba.marathe1@gmail.com>
2024-02-26 09:39:43 +0530

    example on interface added

--- java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Interface/InterfaceDemo2/InterfaceDemo2.java
+++ java-practice/src/com/jotiba/practice/coreJava/oopsConcept/Interface/InterfaceDemo2/InterfaceDemo2.java
@@ -6,45 +6,37 @@ interface Vehicle{
 }
 
 class Car implements Vehicle{
-
-	@Override
+    @Override
 	public void getColor() {
 		System.out.println("Red");
-		
 	}
 
 	@Override
 	public void totalWheels() {
 		System.out.println("Four");
-		
 	}
-	
 }
 
 class Bus implements Vehicle{
-
-	@Override
+    @Override
 	public void getColor() {
 		System.out.println("Blue");
-		
 	}
 
 	@Override
 	public void totalWheels() {
 		System.out.println("Eight");
-		
 	}
-	
 }
 public class InterfaceDemo2{
 	public static void main(String args[]) {
-		Vehicle v1 = new Car();
-		v1.getColor();
-	    v1.totalWheels();
+	  Vehicle v1 = new Car();
+	  v1.getColor();
+	  v1.totalWheels();
 	    
-	    Bus b1 = new Bus();
-		b1.getColor();
-	    b1.totalWheels();
+	  Bus b1 = new Bus();
+	  b1.getColor();
+	  b1.totalWheels();
 	}
 }
 
