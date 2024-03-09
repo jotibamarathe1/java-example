162255c2f423437eda9d1c6a6fd0b5f19cebef40
jotiba <jotiba.marathe1@gmail.com>
2024-03-09 22:16:51 +0530

    length example added

---
+++ java-practice/src/com/jotiba/practice/coreJava/stringMethods/LengthExample.java
@@ -0,0 +1,8 @@
+package com.jotiba.practice.coreJava.strings.stringMethods;
+
+public class LengthExample {
+    public static void main(String[] args) {
+        var message = "Hello ";
+        System.out.println("message.length(): " + message.length());
+    }
+}
